ba3e1b81950ba9f77cf57196a6a07fd3def9705f
dantekadagi3 <dantekadagi3@gmail.com>
2024-06-26 19:33:33 +0300

    basics of dart

---
+++ DART/dart_application_1.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Hello world");
+}
+//comments in dart are written using two backslashes--single line
+//Block comments-These comments take multiple lines at once as shown
+/*
+This is a block comment 
+it takes multiple lines at once
+block comments are designed sometimes to comment out long snipets of code
+*/
+
+//documentation comments also called doc comments 
+///They are created using three backslashes
