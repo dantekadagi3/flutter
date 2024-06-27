0dfd166bff7118e7385a1019b2e89dcbf8207aa0
dantekadagi3 <dantekadagi3@gmail.com>
2024-06-27 14:06:55 +0300

    updates

--- basics of flutter/variables.dart
+++ basics of flutter/variables.dart
@@ -1,3 +1,17 @@
-void main(){
+void main() {
   //variables
+  /*in dart the synatx of writin a variable is 
+  *<datatype> <variablename> =value;
+  There are multiple types of data 
+  */
+  int firstValue = 1;
+  int secondValue = 100;
+   
+  String name = "Dante";
+
+  print(firstValue);
+  print(secondValue);
+  print(name);
+  print(secondValue - firstValue);
+  print(firstValue * secondValue);
 }
