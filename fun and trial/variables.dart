8df2f1d8d643d6cfcf2245c0ee70fff51a9f7c9c
dantekadagi3 <dantekadagi3@gmail.com>
2024-07-01 10:17:12 +0300

    updates

---
+++ fun and trial/variables.dart
@@ -0,0 +1,12 @@
+//creating variables using the var key word
+//when we use var ,Dart automatically perform dynamic typing
+//when you assign a value using var it is not possible to assign a different type of data in the same variable later
+var personName = 'Kashy';
+var personAge = 20;
+const myVar = "Hello World";// we can assign a different type data 
+ // assigning boolean value
+
+void main() {
+  print(personName);
+  print(personAge);
+}
