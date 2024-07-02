3b1dd1eb24cb66fdfdd61c78c2124c23c9426ed0
dantekadagi3 <dantekadagi3@gmail.com>
2024-07-02 18:15:40 +0300

    updates

---
+++ DART/strings.dart
@@ -0,0 +1,14 @@
+void main() {
+  String literal = 'Hello Dante';
+  print(literal);
+
+  //in dart string literals are represented using single or double quotes;
+  //whent you want to represent a multi-line string we use tripple quotes
+
+  String many = '''
+Hello,
+How are you?
+I hope you are having a nice day''';
+
+  print(many);
+}
