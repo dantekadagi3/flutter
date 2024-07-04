c18b603984df0e41e30cf764ba001e8b6619bf49
dantekadagi3 <dantekadagi3@gmail.com>
2024-07-04 19:14:51 +0300

    constants

---
+++ fun and trial/ex2variables/constants.dart
@@ -0,0 +1,22 @@
+void main() {
+  //challenge4
+  /*Declare three constants called rating1 , rating2 and rating3 of type double and assign each
+a value. Calculate the average of the three and store the result in a constant named
+averageRating .*/
+
+  double rating1 = 20.0;
+  double rating2 = 25.0;
+  double rating3 = 21.0;
+
+  double averageRating = (rating1 + rating2 + rating3);
+  print(averageRating);
+
+  //challenge 5
+  /**A quadratic equation is something of the form
+a⋅x² + b⋅x + c = 0 .
+The values of x which satisfy this can be solved by using the equation
+x = (-b ± sqrt(b² - 4⋅a⋅c)) / (2⋅a) .
+Declare three constants named a , b and c of type double . Then calculate the two
+values for x using the equation above (noting that the ± means plus or minus, so one
+value of x for each). Store the results in constants called root1 and root2 of type double . */
+}
